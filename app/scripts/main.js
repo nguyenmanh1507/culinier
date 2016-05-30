ef49286c9c85ee31b200aecc1c06343b73130e7b
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-05-30 16:52:27 +0700

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+// eslint
+(function($) {
+
+  const el = $('#carousel')
+  const prevArrow = '<button type="button"><i class="fa fa-chevron-left"></i></button>'
+  const nextArrow = '<button type="button"><i class="fa fa-chevron-right"></i></button>'
+
+  el.slick({
+    dots: true,
+    mobileFirst: true,
+    prevArrow: prevArrow,
+    nextArrow: nextArrow
+  });
+
+})(jQuery)
