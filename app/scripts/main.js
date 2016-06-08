8d3ae2d324a9d327d4114e3c5b5cd475654a1856
manh <nguyenmanh1507@gmail.com>
2016-06-08 16:30:00 +0700

    update stylesheet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,17 +3,35 @@
 
   if ($(window).slick) {
 
-    const el = $('#carousel')
+    const carousel = $('#carousel')
     const prevArrow = '<button type="button"><i class="fa fa-chevron-left"></i></button>'
     const nextArrow = '<button type="button"><i class="fa fa-chevron-right"></i></button>'
 
-    el.slick({
+    const carouselFor = $('#carouselFor')
+    const carouselNav = $('#carouselNav')
+
+    carousel.slick({
       dots: true,
       mobileFirst: true,
       prevArrow: prevArrow,
       nextArrow: nextArrow
-    });
-    
+    })
+
+    carouselFor.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.carousel-nav'
+    })
+
+    carouselNav.slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.carousel-for',
+      focusOnSelect: true
+    })
+
   }
 
 })(jQuery)
