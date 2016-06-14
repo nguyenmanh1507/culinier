1d96ccc5292ecfc2c78a8e853b933c8d47ac2298
manh <nguyenmanh1507@gmail.com>
2016-06-14 16:57:26 +0700

    support mobile nav

---
+++ app/scripts/navigation.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict'
+
+  const navList = document.querySelector('.site-nav__list')
+  const hamBtn = document.querySelector('.site-nav__ham')
+
+  let isShow = false
+
+  hamBtn.addEventListener('click', function() {
+    if (!isShow) {
+      navList.classList.add('is-show')
+      hamBtn.classList.add('is-nav-open')
+      isShow = !isShow
+    } else {
+      navList.classList.remove('is-show')
+      hamBtn.classList.remove('is-nav-open')
+      isShow = !isShow
+    }
+  })
+
+})()
