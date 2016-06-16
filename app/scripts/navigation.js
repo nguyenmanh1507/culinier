1f2bd6a49d0231294f7a98a56a9817b3b16c4974
manh <nguyenmanh1507@gmail.com>
2016-06-16 09:37:30 +0700

    check btnHam exist

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -6,16 +6,19 @@
 
   let isShow = false
 
-  hamBtn.addEventListener('click', function() {
-    if (!isShow) {
-      navList.classList.add('is-show')
-      hamBtn.classList.add('is-nav-open')
-      isShow = !isShow
-    } else {
-      navList.classList.remove('is-show')
-      hamBtn.classList.remove('is-nav-open')
-      isShow = !isShow
-    }
-  })
+  // Check if hamBtn exist
+  if (hamBtn) {
+    hamBtn.addEventListener('click', function() {
+      if (!isShow) {
+        navList.classList.add('is-show')
+        hamBtn.classList.add('is-nav-open')
+        isShow = !isShow
+      } else {
+        navList.classList.remove('is-show')
+        hamBtn.classList.remove('is-nav-open')
+        isShow = !isShow
+      }
+    })
+  }
 
 })()
